3e62221ff2c32cd866d8b67df5206cf7408d98ae
clementwkm_ <wongkaiman711@gmail.com>
2022-11-18 21:52:54 +0800

    New / Add Tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import Footer from './Footer';
 
 function App() {
   return (
     <div className="App">
+      <h1 className="text-5xl  underline">
+        Hello world!
+      </h1>
+      <Footer />
     </div>
   );
 }
